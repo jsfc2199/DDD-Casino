abf6a19a688d98105a75c32fac470cca800eeb65
Juan Sebastian <jsfc2199@gmail.com>
2022-05-18 12:09:49 -0500

    DDD casino project started

---
+++ src/main/java/com/sofkaU/casinodddproject/CasinoDddProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sofkaU.casinodddproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasinoDddProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasinoDddProjectApplication.class, args);
+	}
+
+}
